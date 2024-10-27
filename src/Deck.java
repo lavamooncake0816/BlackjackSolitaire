89cc033ee44ce96ae4336b902663579da7d4d365
yuchengzhu <yuchengz@seas.upenn.edu>
2024-10-27 14:57:01 -0400

    define an arraylist to hold cards create a constructor that initialises the deck and shuffles it implement a method to deal card

---
+++ src/Deck.java
@@ -0,0 +1,28 @@
+/*
+define an arraylist to hold cards
+create a constructor that initialises the deck and shuffles it
+implement a method to deal card
+ */
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Deck {
+    private ArrayList<Card> cards;
+
+    public Deck() {
+        cards = new ArrayList<>();
+        String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
+        String[] values = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+
+        for (String suit : suits) {
+            for (String value : values) {
+                cards.add(new Card(suit, value));
+            }
+        }
+        Collections.shuffle(cards);
+    }
+
+    public Card dealCard() {
+        return cards.removeLast();
+    }
+}
